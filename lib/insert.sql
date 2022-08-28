bc8660c641319fee654dd945b9f1b5251444754f
Purity Mwaura <purity.mwaura@student.moringaschool.com>
2022-08-28 11:06:50 +0300

    commit tree be487d3bb71e4d999270e335512c7663ffd0b439

---
+++ lib/insert.sql
@@ -0,0 +1,10 @@
+
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Mr. Chocolate", 4, "M", "brown", "unpredictable", FALSE);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Rowdy", 14, "M", "black", "unpredictable", TRUE);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Tabitha", 5, "F", "brown", "unpredictable", TRUE);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Sergeant Brown", 3, "M", "brown", "happy", TRUE);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Melissa", 40, "F", "red", "happy", TRUE);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Grinch", 10, "M", "blue", "angry", TRUE);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Wendy", 1, "F", "gray", "angry", TRUE);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, 5, "M", "white", "nostalgic", FALSE);
